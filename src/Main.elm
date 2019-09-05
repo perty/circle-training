e3c8664904572471ec2efa7485fe7529a2f2b4ad
Per Lundholm <per@artcomputer.se>
2019-09-05 20:07:57 +0200

    Visa förfluten tid sedan man tryckt på start

--- src/Main.elm
+++ src/Main.elm
@@ -1,8 +1,9 @@
 module Main exposing (Message(..), Model, init, main, subscriptions, update, view)
 
 import Browser
-import Html exposing (Html, div, h1, img, p, text)
+import Html exposing (Html, button, div, h1, img, p, text)
 import Html.Attributes exposing (src)
+import Html.Events exposing (onClick)
 import Time
 
 
@@ -12,12 +13,14 @@ import Time
 
 type alias Model =
     { now : Time.Posix
+    , startClicked : Time.Posix
     }
 
 
 init : ( Model, Cmd Message )
 init =
     ( { now = Time.millisToPosix 0
+      , startClicked = Time.millisToPosix 0
       }
     , Cmd.none
     )
@@ -29,6 +32,7 @@ init =
 
 type Message
     = Beat Time.Posix
+    | StartClick
 
 
 update : Message -> Model -> ( Model, Cmd Message )
@@ -36,6 +40,9 @@ update message model =
     case message of
         Beat now ->
             ( { model | now = now }, Cmd.none )
+
+        StartClick ->
+            ( { model | startClicked = model.now }, Cmd.none )
 
 
 
@@ -46,8 +53,9 @@ view : Model -> Html Message
 view model =
     div []
         [ img [ src "/logo.svg" ] []
-        , h1 [] [ text "Time is now" ]
-        , p [] [ text <| String.fromInt <| Time.posixToMillis model.now ]
+        , h1 [] [ text "Förfluten tid" ]
+        , p [] [ text <| String.fromInt <| (Time.posixToMillis model.now - Time.posixToMillis model.startClicked) // 1000 ]
+        , button [ onClick StartClick ] [ text "Start" ]
         ]
 
 
@@ -56,7 +64,7 @@ view model =
 
 
 subscriptions : Model -> Sub Message
-subscriptions model =
+subscriptions _ =
     Time.every 1000 Beat
 
 
